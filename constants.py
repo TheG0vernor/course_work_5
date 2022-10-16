d6bae6c3e329225c4379d53a9e642b2a16ec4fa3
Вадим <niras.sp@gmail.com>
2022-10-16 18:54:02 +0700

    Patch 1.3.7

--- constants.py
+++ constants.py
@@ -2,4 +2,4 @@ import os.path
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-WEAPONS_and_ARMORS = os.path.join(BASE_DIR, 'data\equipment.json')
+WEAPONS_and_ARMORS = os.path.join(BASE_DIR, 'data/equipment.json')
